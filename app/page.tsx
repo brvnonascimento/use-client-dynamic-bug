6045561d5f3576d52c02efb20d81599c1877fbc3
Bruno Nascimento <bruno.hn@icloud.com>
2022-11-20 04:21:45 -0300

    feat: add ssr false flag to prove that it is still being prerendered

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
-const ClientTest = dynamic(() => import("./ClientTest"));
+const ClientTest = dynamic(() => import("./ClientTest"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <div>
-      <Suspense fallback={<h1>Loading Client test!</h1>}>
+      <Suspense fallback={<h1>Loading...</h1>}>
         <ClientTest />
       </Suspense>
     </div>
